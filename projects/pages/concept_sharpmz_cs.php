7d6e8436ba169daef7b6ae1892099f73f1317215
hajdam <hajdam@users.sf.net>
2020-04-11 20:01:17 +0200

    Moved concepts out of download

--- projects/pages/concept_sharpmz_cs.php
+++ projects/pages/concept_sharpmz_cs.php
@@ -3,9 +3,9 @@
 
 <p>Pár konceptů programů pro Sharp MZ-800:</p>
 
-<p><img src="../download/concepts/sharpmz/agent0.gif" alt="[agent0]" width="320" border="2"/>&nbsp;<img src="../download/concepts/sharpmz/agent1.gif" alt="[agent1]" width="320" border="2"/></p>
+<p><img src="images/concepts/sharpmz/agent0.gif" alt="[agent0]" width="320" border="2"/>&nbsp;<img src="images/concepts/sharpmz/agent1.gif" alt="[agent1]" width="320" border="2"/></p>
 
-<p><img src="../download/concepts/sharpmz/monodr0.gif" alt="[monodr0]" width="320" border="2"/>&nbsp;<img src="../download/concepts/sharpmz/monodr1.gif" alt="[monodr1]" width="320" border="2"/></p>
+<p><img src="images/concepts/sharpmz/monodr0.gif" alt="[monodr0]" width="320" border="2"/>&nbsp;<img src="images/concepts/sharpmz/monodr1.gif" alt="[monodr1]" width="320" border="2"/></p>
 
 </div>
 </body>
